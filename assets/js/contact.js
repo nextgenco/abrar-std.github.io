88497e1ee5aa3a34011836d0230ec3233178c7c6
abrar-std <53462275+abrar-std@users.noreply.github.com>
2020-06-10 22:03:37 +0500

    update

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,30 +1,29 @@
 //Contact
-$('#contactForm').submit(function() {
+$("#contactForm").submit(function () {
+  var action = $(this).attr("action");
 
+  console.log(action);
 
-    var action = $(this).attr('action');
+  $("#formMessage").slideUp(750, function () {
+    $("#formMessage").hide();
+    $("#submit").attr("disabled", "disabled");
 
-    console.log(action);
-
-    $("#formMessage").slideUp(750, function() {
-        $('#formMessage').hide();
-        $('#submit').attr('disabled', 'disabled');
-
-        $.post(action, {
-                name: $('#name').val(),
-                email: $('#email').val(),
-                comments: $('#comments').val(),
-            },
-            function(data) {
-                document.getElementById('formMessage').innerHTML = data;
-                $('#formMessage').slideDown('slow');
-                $('#submit').removeAttr('disabled');
-                if (data.match('success') != null) {
-                    console.log("success");
-                }
-            }
-        );
-
-    });
-    return false;
+    $.post(
+      action,
+      {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        comments: $("#comments").val(),
+      },
+      function (data) {
+        document.getElementById("formMessage").innerHTML = data;
+        $("#formMessage").slideDown("slow");
+        $("#submit").removeAttr("disabled");
+        if (data.match("success") != null) {
+          console.log("success");
+        }
+      }
+    );
+  });
+  return false;
 });
